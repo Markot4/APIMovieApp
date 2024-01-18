a3eebd3c3d616a89dd1956eede2c5e87c0509a50
Marko Šeparović <maseparov@vsite.hr>
2024-01-18 18:12:41 +0100

    Add project files.

---
+++ APIMovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using APIMovieApp.Data;
+using APIMovieApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIMovieApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        public MoviesService MoviesService { get; set; }
+        public MoviesController(MoviesService moviesService)
+        {
+            MoviesService = moviesService;
+        }
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] MovieVM movie)
+        {
+            MoviesService.AddMovie(movie);
+            return Ok();
+
+        }
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            var allMovies = MoviesService.GetAllMovies();
+            return Ok(allMovies);
+        }
+        [HttpGet("id")]
+        public IActionResult GetBookById([FromQuery] int id)
+        {
+            var movie = MoviesService.GetMovieById(id);
+            return Ok(movie);
+        }
+        [HttpPut("id")]
+        public IActionResult UpdateMovieById([FromQuery] int id,
+        [FromBody] MovieVM movieVM)
+        {
+            var updatedMovie = MoviesService.UpdateMovieById(id, movieVM);
+            return Ok(updatedMovie);
+        }
+        [HttpDelete("id")]
+        public IActionResult DeleteMovie([FromQuery] int id)
+        {
+            MoviesService.DeleteMovie(id);
+            return Ok();
+        }
+    }
+}
---
+++ APIMovieApp/Services/MoviesService.cs
@@ -0,0 +1,52 @@
+﻿using APIMovieApp.Data;
+
+namespace APIMovieApp.Services
+{
+    public class MoviesService
+    {
+        private AppDbContext _context;
+        public MoviesService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddMovie(MovieVM movie)
+        {
+            var newMovie = new Movie()
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Year = movie.Year,
+                Genre = movie.Genre,
+            };
+            _context.Movies.Add(newMovie);
+            _context.SaveChanges();
+        }
+        public List<Movie> GetAllMovies()
+        {
+            return _context.Movies.ToList();
+        }
+        public Movie GetMovieById(int id)
+        {
+            return _context.Movies.FirstOrDefault(x => x.Id == id);
+        }
+        public Movie UpdateMovieById(int id, MovieVM movieVM)
+        {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            if (movie != null)
+            {
+                movie.Id = movieVM.Id;
+                movie.Name = movieVM.Name;
+                movie.Year = movieVM.Year;
+                movie.Genre = movieVM.Genre;
+                _context.SaveChanges();
+            }
+            return movie;
+        }
+        public void DeleteMovie(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
+    }
+}
